995cb4de2a8cc144eecee8d16a124e3217aec920
ahhmed123 <141465551+ahhmed123@users.noreply.github.com>
2024-08-21 18:38:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document . querySelector('button');
+let div = document . querySelector('div');
+div .style.display ='none';
+button.onclick = () => {
+    div .style.display = 'block';
+};
